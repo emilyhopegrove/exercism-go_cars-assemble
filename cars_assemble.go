88ec5b91798d86a0cc5d0c2c52bd0df78855239f
Emily Grove <eh.grove@yahoo.com>
2023-05-23 15:56:34 -0500

    initial commit

---
+++ cars_assemble.go
@@ -0,0 +1,22 @@
+package cars
+
+// CalculateWorkingCarsPerHour calculates how many working cars are
+// produced by the assembly line every hour.
+func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
+	// panic("CalculateWorkingCarsPerHour not implemented")
+	// successRatePercentage := float64(successRate / 100)
+	// return float64(productionRate) * float64(successRatePercentage)
+}
+
+// CalculateWorkingCarsPerMinute calculates how many working cars are
+// produced by the assembly line every minute.
+func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
+	// panic("CalculateWorkingCarsPerMinute not implemented")
+	// successRatePercentage := float64(successRate / 100)
+
+}
+
+// CalculateCost works out the cost of producing the given number of cars.
+func CalculateCost(carsCount int) uint {
+	panic("CalculateCost not implemented")
+}
