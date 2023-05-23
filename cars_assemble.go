de18e4b833ee940cd90d2bb265ddd1b126525443
Emily Grove <eh.grove@yahoo.com>
2023-05-23 17:20:02 -0500

    working solutions

--- cars_assemble.go
+++ cars_assemble.go
@@ -4,19 +4,37 @@ package cars
 // produced by the assembly line every hour.
 func CalculateWorkingCarsPerHour(productionRate int, successRate float64) float64 {
 	// panic("CalculateWorkingCarsPerHour not implemented")
-	// successRatePercentage := float64(successRate / 100)
-	// return float64(productionRate) * float64(successRatePercentage)
+	return float64(productionRate) * float64(successRate / 100)
 }
 
 // CalculateWorkingCarsPerMinute calculates how many working cars are
 // produced by the assembly line every minute.
 func CalculateWorkingCarsPerMinute(productionRate int, successRate float64) int {
 	// panic("CalculateWorkingCarsPerMinute not implemented")
-	// successRatePercentage := float64(successRate / 100)
+	
+	return int(CalculateWorkingCarsPerHour(productionRate, successRate) / 60)
 
 }
 
 // CalculateCost works out the cost of producing the given number of cars.
+
+/* Check if the carsCount is divisible by 10. 
+This step determines if there are any complete groups of ten cars that can be produced.
+
+	If there are complete groups of ten cars, 
+	calculate the cost by multiplying the number of groups by the cost of producing a group of ten cars, which is $95,000.
+
+If there are any remaining cars that are less than a group of ten, calculate the cost by multiplying the number of remaining cars by the cost of producing a single car, which is $10,000.
+
+Add together the costs from steps 2 and 3 to get the total cost of producing the given number of cars. 
+
+Return the total cost as the result. */
 func CalculateCost(carsCount int) uint {
-	panic("CalculateCost not implemented")
+	costPerGroup := uint(95000)      // Cost of producing 10 cars together
+    costPerCar := uint(10000)        // Cost of producing an individual car
+    groupCount := carsCount / 10     // Number of groups of 10 cars
+    remainingCars := carsCount % 10  // Number of individual cars remaining
+
+    totalCost := uint(groupCount) * costPerGroup + uint(remainingCars) * costPerCar
+    return totalCost
 }
